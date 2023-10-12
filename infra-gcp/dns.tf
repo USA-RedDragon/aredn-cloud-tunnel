a4ffb6bdc63acf4be0c011e10f64a20ba92747a8
Jacob McSwain <jacob.a.mcswain@gmail.com>
2023-10-12 03:16:21 -0500

    add GCP dallas supernode

---
+++ infra-gcp/dns.tf
@@ -0,0 +1,15 @@
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+data "cloudflare_zone" "site-zone" {
+  name = var.domain
+}
+
+resource "cloudflare_record" "record" {
+  zone_id = data.cloudflare_zone.site-zone.id
+  name    = var.subdomain
+  value   = google_compute_address.ip.address
+  type    = "A"
+  proxied = false
+}
