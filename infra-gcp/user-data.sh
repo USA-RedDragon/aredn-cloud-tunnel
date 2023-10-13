7687e1f63065bf7dbdf6c67bd565cc175658109b
Jacob McSwain <jacob.a.mcswain@gmail.com>
2023-10-12 23:32:47 -0500

    gcp: maybe I'll get the ports forwarded correctly

--- infra-gcp/user-data.sh
+++ infra-gcp/user-data.sh
@@ -80,7 +80,7 @@ docker run \
     --device /dev/net/tun \
     --name ${server_name}-supernode \
     -p 5526:5525 \
-    -p 81:3333 \
+    -p 80:81 \
     -d \
     --restart unless-stopped \
     --net aredn-net --ip $NODE_IP_PLUS_1 \
