75a3aa9b6d315a0a967852ae2607839d9ab4adf0
Jacob McSwain <jacob@mcswain.dev>
2025-02-19 19:52:55 -0600

    startup: setup bridges similar to aredn

--- rootfs/usr/bin/start.sh
+++ rootfs/usr/bin/start.sh
@@ -4,15 +4,35 @@
 trap "exit 0" SIGHUP SIGINT SIGTERM
 
 ip link add dev br0 type bridge
-ip address add dev br0 $NODE_IP
 ip link set dev eth0 master br0
 ip link set br0 up
+
+ip link add link br0 name br0.2 type vlan id 2
+ip link add dev br-dtdlink type bridge
+ip link set dev br0.2 master br-dtdlink
+ip address add dev br-dtdlink $NODE_IP/8
+ip link set br-dtdlink up
+
+ip link add link br0 name br0.4 type vlan id 4
+ip link add dev br-wan type bridge
+ip link set dev br0.4 master br-wan
+ip link set br-wan up
+
 GW=$(ip route show default | awk '{ print $3 }')
-ADDR=$(ip address show eth0 | grep 'inet ' | awk '{ print $2 }')
-ip address add dev br0 $ADDR
+
+IPV4_ADDRS=$(ip address show eth0 | grep 'inet ' | awk '{ print $2 }')
+for IPV4_ADDR in $IPV4_ADDRS; do
+    ip address add dev br-wan $IPV4_ADDR
+    ip address del dev eth0 $IPV4_ADDR
+done
+IPV6_ADDRS=$(ip address show eth0 | grep 'inet6 ' | awk '{ print $2 }')
+for IPV6_ADDR in $IPV6_ADDRS; do
+    ip address add dev br-wan $IPV6_ADDR
+    ip address del dev eth0 $IPV6_ADDR
+done
+
 ip route del default
-ip address del dev eth0 $ADDR
-ip route add default via $GW dev br0
+ip route add default via $GW dev br-wan
 
 IS_DISABLE_VTUN=${DISABLE_VTUN:-0}
 
