6933a356c62325bbf57b92cd847eb25276283c81
Jacob McSwain <jacob@mcswain.dev>
2025-02-19 20:00:34 -0600

    startup: adjust addresses

--- rootfs/usr/bin/start.sh
+++ rootfs/usr/bin/start.sh
@@ -10,29 +10,23 @@ ip link set br0 up
 ip link add link br0 name br0.2 type vlan id 2
 ip link add dev br-dtdlink type bridge
 ip link set dev br0.2 master br-dtdlink
-ip address add dev br-dtdlink $NODE_IP/8
 ip link set br-dtdlink up
-
-ip link add link br0 name br0.4 type vlan id 4
-ip link add dev br-wan type bridge
-ip link set dev br0.4 master br-wan
-ip link set br-wan up
 
 GW=$(ip route show default | awk '{ print $3 }')
 
 IPV4_ADDRS=$(ip address show eth0 | grep 'inet ' | awk '{ print $2 }')
 for IPV4_ADDR in $IPV4_ADDRS; do
-    ip address add dev br-wan $IPV4_ADDR
+    ip address add dev br0 $IPV4_ADDR
     ip address del dev eth0 $IPV4_ADDR
 done
 IPV6_ADDRS=$(ip address show eth0 | grep 'inet6 ' | awk '{ print $2 }')
 for IPV6_ADDR in $IPV6_ADDRS; do
-    ip address add dev br-wan $IPV6_ADDR
+    ip address add dev br0 $IPV6_ADDR
     ip address del dev eth0 $IPV6_ADDR
 done
 
 ip route del default
-ip route add default via $GW dev br-wan
+ip route add default via $GW dev br0
 
 IS_DISABLE_VTUN=${DISABLE_VTUN:-0}
 
