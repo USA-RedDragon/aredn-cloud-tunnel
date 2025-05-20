e2df0c807952a3028cd2e96b29b0d162160cd146
Jacob McSwain <jacob@mcswain.dev>
2025-05-19 21:42:30 -0500

    start: rework interfaces

--- rootfs/usr/bin/start.sh
+++ rootfs/usr/bin/start.sh
@@ -5,38 +5,21 @@ set -euxo pipefail
 # Trap signals and exit
 trap "exit 0" SIGHUP SIGINT SIGTERM
 
-ip link add dev br0 type bridge
-ip link set dev eth0 master br0
-ip link set br0 up
-
-# br-lan, vlan 1
-ip link add link br0 name br0.1 type vlan id 1
-ip link add dev br-lan type bridge
-ip link set dev br0.1 master br-lan
-ip link set br0.1 up
-ip link set br-lan up
-
-# br-dtdlink, vlan 2
-ip link add link br0 name br0.2 type vlan id 2
-ip link add dev br-dtdlink type bridge
-ip link set dev br0.2 master br-dtdlink
-ip link set br0.2 up
-ip link set br-dtdlink up
-
-# br-wan, vlan 4
-ip link add link br0 name br0.4 type vlan id 4
 ip link add dev br-wan type bridge
-ip link set dev br0.4 master br-wan
-ip link set br0.4 up
+ip link set dev eth0 master br-wan
 ip link set br-wan up
 
-GW=$(ip route show default | awk '{ print $3 }')
-IP6_GW=$(ip -6 route show default | awk '{ print $3 }')
+ip link add dev br-dtdlink type bridge
+ip link set dev eth1 master br-dtdlink
+ip link set br-dtdlink up
+
+GW=$(ip route list 0/0 | awk '{ print $3 }')
+IP6_GW=$(ip -6 route list ::/0 | awk '{ print $3 }')
 
 IPV4_ADDRS=$(ip address show eth0 | grep 'inet ' | awk '{ print $2 }')
 for IPV4_ADDR in $IPV4_ADDRS; do
     ip address del dev eth0 $IPV4_ADDR
-    ip address add dev br0 $IPV4_ADDR
+    ip address add dev br-wan $IPV4_ADDR
 done
 IPV6_ADDRS=$(ip address show eth0 | grep 'inet6 ' | awk '{ print $2 }')
 for IPV6_ADDR in $IPV6_ADDRS; do
@@ -45,11 +28,12 @@ for IPV6_ADDR in $IPV6_ADDRS; do
         continue
     fi
     ip address del dev eth0 $IPV6_ADDR
-    ip address add dev br0 $IPV6_ADDR
+    ip address add dev br-wan $IPV6_ADDR
 done
 
-ip route add default via $GW dev br0
-# ip -6 route add default via $IP6_GW dev br-wan
+ip route add default via $GW dev br-wan
+ip -6 route del default via $IP6_GW dev eth0
+ip -6 route add default via $IP6_GW dev br-wan
 
 SUPERNODE=${SUPERNODE:-}
 if [ -n "$SUPERNODE" ]; then
@@ -57,7 +41,6 @@ if [ -n "$SUPERNODE" ]; then
 fi
 
 ip address add dev br-dtdlink $NODE_IP/8
-# ip address add dev br-lan $LAN_IP/29
 
 mkdir -p /etc/arednlink
 echo "${NODE_IP} ${SERVER_NAME}" >> /etc/arednlink/hosts
